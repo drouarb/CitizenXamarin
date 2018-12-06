47fd144cfddcbdfed8f16680a9074ec3e1bdc931
Emile JONAS <emile.jonas@epitech.eu>
2018-12-06 06:26:09 +0100

    notification service added (untested, auth app down)

---
+++ citizen/Models/Api/NotificationItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace citizen.Models.Api
+{
+    class NotificationItem
+    {
+        [JsonProperty("uuid")]
+        public Guid Uuid { get; set; }
+        [JsonProperty("userUuid")]
+        public Guid UserUuid { get; set; }
+        [JsonProperty("title")]
+        public string Title { get; set; }
+        [JsonProperty("content")]
+        public string Content { get; set; }
+        [JsonProperty("created")]
+        public DateTime Created { get; set; }
+        [JsonProperty("viewed")]
+        public Boolean Viewed { get; set; }
+        [JsonProperty("url")]
+        public string Url { get; set; }
+    }
+}
---
+++ citizen/Services/NotificationService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using citizen.Models.Api;
+using Newtonsoft.Json;
+
+namespace citizen.Services
+{
+    class NotificationService
+    {
+        private List<NotificationItem> notifications;
+
+        public async Task<IEnumerable<NotificationItem>> GetNotificationsAsync(bool forceRefresh = false)
+        {
+            if (forceRefresh == false && notifications.Count != 0)
+                return notifications;
+
+            string rawThreads = await App.ApiService.ApiRequest("https://citizen.navispeed.eu/api/notification/unread", HttpMethod.Get, null);
+            Console.WriteLine("raw threads:" + rawThreads);
+            notifications = JsonConvert.DeserializeObject<List<NotificationItem>>(rawThreads);
+            Console.WriteLine("threads count" + notifications.Count);
+            notifications.ForEach(thread =>
+            {
+                Console.WriteLine(thread.Title);
+                Console.WriteLine(thread.Created);
+            });
+            return notifications;
+        }
+    }
+}
