4480b21106a0f17ab5cb43ad1698e80d1371bd2d
Benjamin DROUARD <benjamin.drouard@epitech.eu>
2018-12-10 16:37:03 +0100

    Design news

--- citizen/Views/NewsDetailsView.xaml.cs
+++ citizen/Views/NewsDetailsView.xaml.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
@@ -26,7 +27,19 @@ namespace citizen.Views
 		{
 			var browser = new WebView();
 			var htmlSource = new HtmlWebViewSource();
-			htmlSource.Html = "<h2>" + viewModel.news.title + "</h2>" + "<h3>" + viewModel.news.subtitle + "</h3>" + viewModel.news.content;
+			htmlSource.Html = "<html>" +
+			                  "		<head>" +
+			                  "			<link href='https://v1.grommet.io/assets/css/index-vanilla.css' rel='stylesheet' type='text/css'>" +
+			                  "			<link href='http://192.168.1.30/style.css' rel='stylesheet' type='text/css'>" +
+			                  "		</head>" +
+			                  "		<body>" +
+			                  "		<div style='width:90%;margin: 0 auto;' >" +
+			                  "			<h1 class='grommetux-heading grommetux-heading--align-center'>" + viewModel.news.title + "</h2>" +
+			                  "			<h2 class='grommetux-heading grommetux-heading--align-center'>" + viewModel.news.subtitle + "</h3>" +
+			                  Regex.Replace(viewModel.news.content, @"\<img ", "<img style='width:110%;margin-left:-5%;' ") +
+			                  "		</div>" +
+			                  "		</body>" +
+			                  "</html>";
 			browser.Source = htmlSource;
 			Content = browser;
 		}
